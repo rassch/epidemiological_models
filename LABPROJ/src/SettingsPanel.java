333015c3f7828631ab0f343b846dff7a9fa2d296
rassch <274426@pw.edu.pl>
2018-03-31 22:12:14 +0200

    Added labels and a button.

--- LABPROJ/src/SettingsPanel.java
+++ LABPROJ/src/SettingsPanel.java
@@ -5,13 +5,20 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class SettingsPanel extends JPanel { //TO DO dodać labele i actionListenery
 	
 	JSlider sick_days = new JSlider(0,25,7);
 	JSlider transm_rate = new JSlider(0,100,10);
 	JSlider death_rate = new JSlider(0,100,0);
+	JLabel sick_days_label = new JLabel ("sick(days)");
+	JLabel transm_rate_label = new JLabel ("transmission rate (%)");
+	JLabel death_rate_label = new JLabel ("death rate (%)");
+	JLabel disease_settings_label = new JLabel ("SIMULATION SETTINGS");
 	JRadioButton asynchronous = new JRadioButton("asynchronous");
+	JButton play = new JButton("GO!");
 	
 	
 	SettingsPanel(){
@@ -30,6 +37,7 @@ public class SettingsPanel extends JPanel { //TO DO dodać labele i actionListen
 		GroupLayout.Group lineO6 = objPropertiesPanelLayout.createParallelGroup();
 		GroupLayout.Group lineO7 = objPropertiesPanelLayout.createParallelGroup();
 		GroupLayout.Group lineO8 = objPropertiesPanelLayout.createParallelGroup();
+		GroupLayout.Group lineO9 = objPropertiesPanelLayout.createParallelGroup();
 		//
 		GroupLayout.SequentialGroup linesO = objPropertiesPanelLayout.createSequentialGroup();
 		GroupLayout.SequentialGroup columnsO = objPropertiesPanelLayout.createSequentialGroup();
@@ -41,22 +49,38 @@ public class SettingsPanel extends JPanel { //TO DO dodać labele i actionListen
 		linesO.addGroup(lineO6);
 		linesO.addGroup(lineO7);
 		linesO.addGroup(lineO8);
+		linesO.addGroup(lineO9);
 		columnsO.addGroup(columnO1);
 		//
 		objPropertiesPanelLayout.setHorizontalGroup(columnsO);
 		objPropertiesPanelLayout.setVerticalGroup(linesO);
 		
-		lineO2.addComponent(sick_days);
+		lineO1.addComponent(disease_settings_label);
+		columnO1.addComponent(disease_settings_label);
+		
+		lineO2.addComponent(sick_days_label);
+		columnO1.addComponent(sick_days_label);
+		
+		lineO3.addComponent(sick_days);
 		columnO1.addComponent(sick_days);
 		
-		lineO4.addComponent(transm_rate);
+		lineO4.addComponent(transm_rate_label);
+		columnO1.addComponent(transm_rate_label);
+		
+		lineO5.addComponent(transm_rate);
 		columnO1.addComponent(transm_rate);
 		
-		lineO6.addComponent(death_rate);
+		lineO6.addComponent(death_rate_label);
+		columnO1.addComponent(death_rate_label);
+		
+		lineO7.addComponent(death_rate);
 		columnO1.addComponent(death_rate);
 		
 		lineO8.addComponent(asynchronous);
 		columnO1.addComponent(asynchronous);
+		
+		lineO9.addComponent(play);
+		columnO1.addComponent(play);
 		
 		
 		//suwak do wybierania dni bycia chorym
@@ -79,7 +103,7 @@ public class SettingsPanel extends JPanel { //TO DO dodać labele i actionListen
 		death_rate.setPaintLabels(true);
 	
 		
-		this.setBackground(Color.BLUE);	
+		this.setBackground(new Color(204,255,255));
 		//this.add(sick_days);
 		this.add(transm_rate);
 		this.add(death_rate);
