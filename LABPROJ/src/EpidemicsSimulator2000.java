f252e376ea8e8e1536c5c46a148c452108c745f3
Carlito <Carlito@DonCarlito>
2018-04-11 20:27:53 +0200

    Fixed problems

--- LABPROJ/src/EpidemicsSimulator2000.java
+++ LABPROJ/src/EpidemicsSimulator2000.java
@@ -1,3 +1,5 @@
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 
 public class EpidemicsSimulator2000 {
@@ -6,6 +8,7 @@ public class EpidemicsSimulator2000 {
 		
 		Human czlek = new Human(2,1, "susceptible"); //test klasy human
 		WindowGUI frame = new WindowGUI(); //Tworzenie okna 
+		
 		
 	}
 
--- LABPROJ/src/WindowGUI.java
+++ LABPROJ/src/WindowGUI.java
@@ -5,12 +5,11 @@ import javax.swing.JPanel;
 
 public class WindowGUI extends JFrame {{
 	
-	AnimationPanel animation = new AnimationPanel();
 	SettingsPanel settings = new SettingsPanel(); // tworzenie paneli
 	this.setTitle("EpidemicsSimulator2000"); //tytuł
 	this.setLayout(new BorderLayout());
 	this.add(settings, BorderLayout.EAST);
-	this.add(animation, BorderLayout.WEST);
+	
 	 // dodanie paneli do okna
 	
 	
